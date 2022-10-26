00e830d428830621436c5727ecc6448e23a2b703
amilson gomes <gomes.amilson@gmail.com>
2022-10-26 11:55:34 -0300

    .

--- lib/components/botton_card.dart
+++ lib/components/botton_card.dart
@@ -7,8 +7,9 @@ import '../controller/user_controller.dart';
 import '../pages/repository_list_page.dart';
 
 class BottonCard extends StatefulWidget {
-String? text;
-BottonCard(this.text, {Key? key}) : super(key: key);
+  String? text;
+
+  BottonCard(this.text, {Key? key}) : super(key: key);
 
   @override
   State<BottonCard> createState() => _BottonCardState();
@@ -56,90 +57,85 @@ class _BottonCardState extends State<BottonCard> {
 
   _bottonCard(BuildContext context, UserController provider) {
     return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Card(
-                  elevation: 5,
-                  child: SizedBox(
-                    height: 165,
-                    width: MediaQuery.of(context).size.width,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 25),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.receipt,
-                                size: 20,
-                                color: Colors.white,
-                              ),
-                              const SizedBox(width: 20),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              RepositoryListPage(widget.text)));
-                                },
-                                child: const Text(
-                                  StringConstants.repository,
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(width: 150),
-                              Text(
-                                  provider.decodeJson[
-                                          ServiceConstants.publicRepository]
-                                      .toString(),
-                                  style: const TextStyle(fontSize: 18)),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            children: const [
-                              Icon(Icons.dataset_outlined,
-                                  color: Colors.orange),
-                              SizedBox(width: 20),
-                              Text(
-                                StringConstants.organizations,
-                                style: TextStyle(
-                                    fontSize: 20, color: Colors.white),
-                              ),
-                              SizedBox(width: 135),
-                              Text(
-                                '0',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            children: const [
-                              Icon(Icons.star, color: Colors.yellow),
-                              SizedBox(width: 15),
-                              Text(
-                                StringConstants.ratedAsStar,
-                                style: TextStyle(
-                                    fontSize: 20, color: Colors.white),
-                              ),
-                              SizedBox(width: 40),
-                              Text(
-                                '17',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ],
-                          ),
-                        ],
+      padding: const EdgeInsets.all(5.0),
+      child: Card(
+        elevation: 5,
+        child: SizedBox(
+          height: 165,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.receipt,
+                      size: 20,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(width: 20),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    RepositoryListPage(widget.text)));
+                      },
+                      child: const Text(
+                        StringConstants.repository,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
+                    const SizedBox(width: 150),
+                    Text(
+                        provider.decodeJson[ServiceConstants.publicRepository]
+                            .toString(),
+                        style: const TextStyle(fontSize: 18)),
+                  ],
                 ),
-              );
+                const SizedBox(height: 20),
+                Row(
+                  children: const [
+                    Icon(Icons.dataset_outlined, color: Colors.orange),
+                    SizedBox(width: 20),
+                    Text(
+                      StringConstants.organizations,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    SizedBox(width: 135),
+                    Text(
+                      '0',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: const [
+                    Icon(Icons.star, color: Colors.yellow),
+                    SizedBox(width: 15),
+                    Text(
+                      StringConstants.ratedAsStar,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      '17',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/components/card_list.dart
+++ lib/components/card_list.dart
@@ -5,6 +5,7 @@ import '../controller/popupar_repository_controller.dart';
 
 class ListRepository extends StatefulWidget {
   String? text;
+
   ListRepository(this.text, {Key? key}) : super(key: key);
 
   @override
@@ -25,92 +26,94 @@ class _ListRepositoryState extends State<ListRepository> {
 
   @override
   Widget build(BuildContext context) {
-    final PopularRepositoryController providerRepository =
-        Provider.of<PopularRepositoryController>(context);
+    final  providerRepository = Provider.of<PopularRepositoryController>(context);
     if (providerRepository.list.isEmpty) {
       return const Center(child: CircularProgressIndicator());
     } else {
-      return Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: SizedBox(
-          height: 180,
-          width: double.infinity,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: providerRepository.list.length,
-              itemBuilder: (context, index) {
-                var listRepository = providerRepository.list[index];
-                return InkWell(
-                  onTap: CallRepository(repositoyModels: listRepository)
-                      .callGithub,
-                  child: Card(
-                    elevation: 5,
-                    child: SizedBox(
-                      height: 230,
-                      width: 280,
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                CircleAvatar(
-                                  radius: 18,
-                                  backgroundImage: NetworkImage(listRepository
-                                      .owner!.avatarUrl
-                                      .toString()),
-                                ),
-                                const SizedBox(width: 10),
-                                Text(
-                                  listRepository.owner!.login.toString(),
-                                  style: const TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                )
-                              ],
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              listRepository.name.toString(),
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+      return _repositoryList(providerRepository);
+    }
+  }
+
+  _repositoryList(PopularRepositoryController providerRepository) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: SizedBox(
+        height: 180,
+        width: double.infinity,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: providerRepository.list.length,
+            itemBuilder: (context, index) {
+              var listRepository = providerRepository.list[index];
+              return InkWell(
+                onTap:
+                    CallRepository(repositoyModels: listRepository).callGithub,
+                child: Card(
+                  elevation: 5,
+                  child: SizedBox(
+                    height: 230,
+                    width: 280,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 18,
+                                backgroundImage: NetworkImage(
+                                    listRepository.owner!.avatarUrl.toString()),
                               ),
+                              const SizedBox(width: 10),
+                              Text(
+                                listRepository.owner!.login.toString(),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            listRepository.name.toString(),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
-                            const SizedBox(height: 5),
-                            Text(listRepository.description.toString()),
-                            const SizedBox(height: 25),
-                            Row(
-                              children: [
-                                const Icon(Icons.star, color: Colors.yellow),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Text(listRepository.stargazersCount.toString()),
-                                const SizedBox(
-                                  width: 15,
-                                ),
-                                Container(
-                                  height: 15,
-                                  width: 15,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(50),
-                                      color: Colors.lightBlue),
-                                ),
-                                const SizedBox(width: 10),
-                                Text(listRepository.language.toString()),
-                              ],
-                            ),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(listRepository.description.toString()),
+                          const SizedBox(height: 25),
+                          Row(
+                            children: [
+                              const Icon(Icons.star, color: Colors.yellow),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(listRepository.stargazersCount.toString()),
+                              const SizedBox(
+                                width: 15,
+                              ),
+                              Container(
+                                height: 15,
+                                width: 15,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.lightBlue),
+                              ),
+                              const SizedBox(width: 10),
+                              Text(listRepository.language.toString()),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                );
-              }),
-        ),
-      );
-    }
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
