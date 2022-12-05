4b52b101196ae86b1d5b2f37c087c8fb670c9156
amilson gomes <gomes.amilson@gmail.com>
2022-12-05 11:01:03 -0300

    alterado para pasta service

--- lib/service/user_service.dart
+++ lib/service/user_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import '../constants/error_constants.dart';
 
 class UserController extends ChangeNotifier {
-  var decodeJson;
+  late Map<String,dynamic> decodeJson;
 
   Future<Map<String, dynamic>> getUser(context,String user) async {
     try {
@@ -12,7 +12,7 @@ class UserController extends ChangeNotifier {
         var url = Uri.parse('https://api.github.com/users/$user');
         var response = await http.get(url);
         if (response.statusCode == 200) {
-          decodeJson = jsonDecode(response.body);
+         decodeJson = jsonDecode(response.body);
           notifyListeners();
         }
       }else{
