fb75a5a06ffb80feb5d3268d3de166fa8f44179e
amilson gomes <gomes.amilson@gmail.com>
2022-12-09 12:29:12 -0300

    alterado variaveis

--- lib/service/repository_service.dart
+++ lib/service/repository_service.dart
@@ -7,6 +7,9 @@ import '../models/repository_model.dart';
 class RepositoryController extends ChangeNotifier {
   List<RepositoyModels> list = [];
   List<RepositoyModels> lista = [];
+
+  List<RepositoyModels> listRepo = [];
+  List<RepositoyModels> listaRepo = [];
   Future<List<RepositoyModels>> getRepository(String? user) async {
     try {
       var url = Uri.parse('https://api.github.com/users/$user/repos');
@@ -23,5 +26,22 @@ class RepositoryController extends ChangeNotifier {
       return lista;
     }
     return [];
+  }
+  Future<List<RepositoyModels>> searchRepository({ String? user,required String query}) async {
+    try {
+      var url = Uri.parse('https://api.github.com/users/$user/repos');
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var decodeJson = jsonDecode(response.body);
+        decodeJson.forEach((item) => listRepo.add(RepositoyModels.fromJson(item)));
+        listaRepo = listRepo.where((item) => item.description!.toLowerCase().contains(query.toLowerCase())).toList();
+        notifyListeners();
+        return listRepo;
+      }
+    } catch (e) {
+      print('${ErrorConstants.erroAoAcessarPagina}$e');
+      return listaRepo;
+    }
+    return lista;
   }
 }
